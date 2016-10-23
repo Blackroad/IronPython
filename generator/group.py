a719dd0639feb2c3f7a10066a7ee724ad50483d6
Vladyslav Velychko <VladVell@gmail.com>
2016-10-23 21:38:02 +0300

    change

--- generator/group.py
+++ generator/group.py
@@ -1,13 +1,15 @@
-from Model.group import Group
+from model.group import Group
 import random
 import string
 import os.path
 import getopt
 import sys
+import time
 
 
 import clr
-clr.AddReferenceByName('')
+clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
+from Microsoft.Office.Interop import Excel
 
 
 try:
@@ -42,4 +44,14 @@ testdata = [Group(name=random_string('name',3,symbols=1))
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..",f)
 
+excel = Excel.ApplicationClass()
+excel.Visible = True
+workbook = excel.Workbooks.Add()
+sheet = workbook.ActiveSheet
 
+for i in range(len(testdata)):
+    sheet.Range["A%s" % (i+1)].Value2 = testdata[i].name
+
+workbook.SaveAs(file)
+time.sleep(10)
+excel.Quit()
--- test_something.py
+++ test_something.py
@@ -1,2 +1,20 @@
+import clr
+import os.path
+project_dir = os.path.dirname(os.path.abspath(__file__))
+import sys
+sys.path.append(os.path.join(project_dir, "TestStack.White.9.2.0.11\\lib\\net40\\"))
+sys.path.append(os.path.join(project_dir, "Castle.Core.3.1.0\\lib\\net40-client\\"))
+clr.AddReferenceByName('TestStack.White')
+
+from TestStack.White import Application
+from TestStack.White.UIItems.Finders import *
+
+
 def test_something():
-    print('ok')
+    application = Application.Launch('c:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook.exe')
+    main_window = application.GetWindow("Free Address Book")
+    main_window.Get(SearchCriteria.ByAutomationId("groupButton")).Click()
+    modal = main_window.ModalWindow('Group editor')
+
+    modal.Get(SearchCriteria.ByAutomationId("uxCloseAddressButton")).Click()
+    main_window.Get(SearchCriteria.ByAutomationId("uxExitAddressButton")).Click()
