504d6fd52845ff10d057d7ac06189fb122831b31
Blackroad <VladVell@gmail.com>
2016-10-26 12:01:51 +0300

    Stuck

--- test_something.py
+++ test_something.py
@@ -7,14 +7,8 @@ sys.path.append(os.path.join(project_dir, "Castle.Core.3.1.0\\lib\\net40-client\
 clr.AddReferenceByName('TestStack.White')
 
 from TestStack.White import Application
-from TestStack.White.UIItems.Finders import *
+
 
 
 def test_something():
-    application = Application.Launch('c:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook.exe')
-    main_window = application.GetWindow("Free Address Book")
-    main_window.Get(SearchCriteria.ByAutomationId("groupButton")).Click()
-    modal = main_window.ModalWindow('Group editor')
-
-    modal.Get(SearchCriteria.ByAutomationId("uxCloseAddressButton")).Click()
-    main_window.Get(SearchCriteria.ByAutomationId("uxExitAddressButton")).Click()
+     Application.Launch('notepad.exe')
