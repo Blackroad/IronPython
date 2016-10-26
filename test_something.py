c2619ff3d0c3df77224cdddf4350be42e268878c
Blackroad <VladVell@gmail.com>
2016-10-26 20:20:35 +0300

    test 2

--- test_something.py
+++ test_something.py
@@ -3,12 +3,18 @@ import os.path
 project_dir = os.path.dirname(os.path.abspath(__file__))
 import sys
 sys.path.append(os.path.join(project_dir, "TestStack.White.0.13.3\\lib\\net40"))
-sys.path.append(os.path.join(project_dir, "Castle.Core.3.3.0\\lib\\net40-client"))
+sys.path.append(os.path.join(project_dir, "Castle.Core.3.3.0\\lib\\net45"))
 clr.AddReferenceByName('TestStack.White')
+clr.AddReference("White.NUnit")
 
 from TestStack.White import Application
-
+from TestStack.White.UIItems.Finders import *
 
 
 def test_something():
-     Application.Launch('notepad.exe')
+     application = Application.Launch('c:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook.exe')
+     main_window = application.GetWindow("Free Address Book")
+     main_window.Get(SearchCriteria.ByAutomationId("groupButton")).Click()
+     modal = main_window.ModalWindow('Group editor')
+     modal.Get(SearchCriteria.ByAutomationId("uxCloseAddressButton")).Click()
+     main_window.Get(SearchCriteria.ByAutomationId("uxExitAddressButton")).Click()
